29521372e1c32069c29cdf51e484e4e7e48660a3
giegre <giegre@github.com>
2018-03-20 22:37:19 +0700

    initial push

---
+++ src/components/PokeList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ListGroup, ListGroupItem, Col } from 'react-bootstrap/lib/';
+
+const PokeList = ({ListOfPokemon}) => {
+
+  let pokemon = ListOfPokemon.map((creature) => {
+    return(
+      <Col sm={6} md={4} key={creature.name}>
+        <ListGroupItem className='Pokelist-item'>{creature.name}</ListGroupItem>
+      </Col>
+    )
+  });
+
+  return(
+    <ListGroup>
+      {pokemon}
+    </ListGroup>
+  )
+
+}
+
+export default PokeList;
